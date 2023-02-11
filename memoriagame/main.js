d7d8ddb401319584f1eced3f31ed3a18d15c539b
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-02-11 01:48:15 -0300

    Update main.js

--- memoriagame/main.js
+++ memoriagame/main.js
@@ -34,24 +34,6 @@ const cronometro = `<p><span id="horas">00</span>:<span id="minutos">00</span>:<
 <button id="pausar">Pausar</button>
 <button id="resetar">Resetar</button>`
 
-const setCSSVar = (name, value, element = document.documentElement) => {
-    element.style.setProperty(`--${name}`, value);
-}
-
-const removeCSSVar = (name, element = document.documentElement) => {
-    element.style.removeProperty(`--${name}`);
-}
-
-// remover atributo src da img que não tem link local ou remoto
-const reset_if_image_is_null = (nameClass) => {
-    elements = document.getElementsByClassName(nameClass);
-    for (let i = 0; i <elements.length; i++) {
-        if(elements[i].getAttribute('src') == "") {
-           return elements[i].removeAttribute('src') //.style.backgroundColor=colorValue; // return elements[i].setAttribute('style', colorValue); // document.getElementById('tst').removeAttribute('src'); 
-         }
-    }
-}
-
 const createElement = (tag, className) => {
   const element = document.createElement(tag);
   element.className = className;
